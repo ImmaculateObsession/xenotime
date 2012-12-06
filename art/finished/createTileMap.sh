112b6492d636b4895344db2dcdb178821f4b78a5
Allison Carlisle <acarlisl@soe.ucsc.edu>
2012-12-06 01:03:41 -0800

    Finished the create tile map script and used it to generate the new tile map.

--- art/finished/createTileMap.sh
+++ art/finished/createTileMap.sh
@@ -6,6 +6,7 @@ MAX_WIDTH=768
 
 if [[ $# != 2 ]]; then
   echo $0 export_list.txt tilemap_out.png
+  exit 1
 fi
 
 EXP_LIST=$1
@@ -14,9 +15,13 @@ OUTFILE=$2
 OUTPUT_ORDER=""
 for IMAGE in `cat $EXP_LIST`; do
    convert -crop 64x64 $IMAGE auto_gen/$IMAGE
-   OUTPUT_ORDER=$OUTPUT_ORDER `ls auto_gen/${IMAGE%%.*}*.png`
+   OUTPUT_ORDER=$OUTPUT_ORDER" "auto_gen/${IMAGE%%.*}*.png
 done
-NUMBER_IMAGES=`echo $OUTPUT_ORDER | wc -c`
-HEIGHT=$(((1+(NUMBER_IMAGES/(MAX_WIDTH/64)))*64))
+NUMBER_IMAGES=0
+for I in $OUTPUT_ORDER; do
+   NUMBER_IMAGES=$((NUMBER_IMAGES+1))
+done
+HEIGHT=$(((1+(NUMBER_IMAGES/(MAX_WIDTH/64)))))
+WIDTH=$((MAX_WIDTH/64))
 
-montage $OUTPUT_ORDER -geometry $WIDTH"x"$HEIGHT $OUTFILE
+montage $OUTPUT_ORDER -tile $WIDTH"x"$HEIGHT -geometry 64x64 -background none $OUTFILE
